1ee1de2efa5c7a7b3f5bacbefb8043ab4ca8b898
Huszar Beata <beata.huszar@adevinta.com>
2020-11-14 18:57:44 +0100

    Add background to menu

--- src/controllers/screenControllers/MenuController.js
+++ src/controllers/screenControllers/MenuController.js
@@ -1,6 +1,9 @@
 import { CANVAS_HEIGHT, CANVAS_WIDTH, TEXT_STYLE } from '../../helpers/globals';
+import BackgroundModel from '../../models/BackgroundModel';
 import TextModel from '../../models/TextModel';
 import ScreenController from './ScreenController';
+import background from '../../assets/images/space_background_pack/layers/parallax-space-stars.png';
+import planets from '../../assets/images/space_background_pack/layers/parallax-space-far-planets.png';
 
 export default class MenuController extends ScreenController {
   constructor({ stage, nextScreen = '' }) {
@@ -13,6 +16,21 @@ export default class MenuController extends ScreenController {
       positionY: CANVAS_HEIGHT * 0.2,
       style: { ...TEXT_STYLE },
     });
-    this.addChild(this.title);
+    this.background = new BackgroundModel({
+      texture: background,
+      width: CANVAS_WIDTH,
+      height: CANVAS_HEIGHT,
+    });
+    this.planets = new BackgroundModel({
+      texture: planets,
+      width: CANVAS_WIDTH,
+      height: CANVAS_HEIGHT,
+    });
+    this.addChild(this.background, this.planets, this.title);
+  }
+
+  gameLoop() {
+    this.background.tilePosition.x += 0.3;
+    this.planets.tilePosition.x += 0.1;
   }
 }
