f03481a7dc1b2dc2ffb2a0fa5b183754a24d4f68
Huszar Beata <beata.huszar@adevinta.com>
2020-11-14 22:08:40 +0100

    Register InteractionManager

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { Renderer, BatchRenderer } from '@pixi/core';
 import { TickerPlugin } from '@pixi/ticker';
 import { AppLoaderPlugin } from '@pixi/loaders';
 import { TilingSpriteRenderer } from '@pixi/sprite-tiling';
+import { InteractionManager } from '@pixi/interaction';
 import App from './controllers/App';
 
 Renderer.registerPlugin('batch', BatchRenderer);
 Renderer.registerPlugin('tilingSprite', TilingSpriteRenderer);
+Renderer.registerPlugin('interaction', InteractionManager);
 Application.registerPlugin(TickerPlugin);
 Application.registerPlugin(AppLoaderPlugin);
 
